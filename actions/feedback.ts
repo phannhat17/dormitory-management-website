4afa95235bb1417afa9e396da8c80253cc0fe18c
hieunt210330 <135931841+hieunt210330@users.noreply.github.com>
2024-04-17 16:29:16 +0000

    Add check permision to server actions

--- actions/feedback.ts
+++ actions/feedback.ts
@@ -5,6 +5,7 @@ import { FeedbackSchema } from "@/schemas";
 import escapeHtml from "escape-html";
 import { auth } from "@/auth";
 import { db } from "@/lib/db";
+import {checkAdmin} from "./check-permission";
 
 export const feedback = async (values: z.infer<typeof FeedbackSchema>) => {
   const session = await auth();
@@ -30,4 +31,20 @@ export const feedback = async (values: z.infer<typeof FeedbackSchema>) => {
     },
   });
   return { success: "Feedback sent!" };
-}
+};
+
+export const deleteFeedback = async (id: number) => {
+  const isAdmin = await checkAdmin();
+
+  if (isAdmin) {
+    console.log("work")
+    return { success: "Feedback deleted successfully" };
+  }
+  // await db.feedback.delete({
+  //   where: {
+  //     id: id,
+  //   },
+  // });
+  return { error: "You must be an admin to delete feedback!" };
+
+};
